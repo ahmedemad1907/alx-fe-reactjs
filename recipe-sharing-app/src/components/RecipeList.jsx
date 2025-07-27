c978155f236b96f2b7552f6bfb9f1890c3d29d8e
Ahmed Emad <ahmedemad20021907@gmail.com>
2025-07-27 11:29:28 +0300

    Fix RecipeList implementation

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,24 @@
-import { useRecipeStore } from '../stores/recipeStore';
+import React from 'react';
+import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipes</h2>
+      {recipes.length === 0 ? (
+        <p>No recipes yet.</p>
+      ) : (
+        <ul>
+          {recipes.map((recipe, index) => (
+            <li key={index}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
