37348380ce4612c6827d98aaa8ba8618f955ccd1
Ahmed Emad <ahmedemad20021907@gmail.com>
2025-07-27 11:26:41 +0300

    Fix: implement zustand store and required components

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
